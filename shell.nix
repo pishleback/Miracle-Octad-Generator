3ee1459c5627cc3657c8f2a52b339017458d6227
Michael <michael@otopia.co.uk>
2026-06-30 19:54:38 +0100

    functions enumeration

--- shell.nix
+++ shell.nix
@@ -17,21 +17,27 @@
 in
   pkgs.mkShell rec {
     buildInputs = with pkgs; [
+      rustc
+      cargo
+      gcc
+      rustfmt
+      clippy
       clang
       # Replace llvmPackages with llvmPackages_X, where X is the latest LLVM version
       llvmPackages_21.bintools
-      rustup
       openssl
       pkg-config
       gcc
     ];
+
+    # Certain Rust tools won't work without this
+    # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
+    # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
+    RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
     RUSTC_VERSION = overrides.toolchain.channel;
     # https://github.com/rust-lang/rust-bindgen#environment-variables
     LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
-    shellHook = ''
-      export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-      export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
-      '';
     # Add precompiled library to rustc search path
     RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
       # add libraries here (e.g. pkgs.libvmi)
