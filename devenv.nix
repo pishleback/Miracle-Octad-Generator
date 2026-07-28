b58cf331c4b686050260f1b33ba41c81bd0e415a
Michael <michael@otopia.co.uk>
2026-07-28 22:38:26 +0100

    devenv processes for wasm serve

--- devenv.nix
+++ devenv.nix
@@ -4,9 +4,11 @@
   languages.rust = {
     enable = true; 
     channel = "stable";
+    targets = [ "wasm32-unknown-unknown" ];
   };
 
   packages = with pkgs; [
+    trunk
     libX11
     libXcursor
     libXrandr
@@ -28,4 +30,36 @@
     pkgs.wayland
     pkgs.libxkbcommon
   ];
+
+  processes.wasm_debug = {
+    exec = "trunk serve --port 8080";
+    ready = {
+      http.get = {
+        port = 8080;
+        path = "/";
+      };
+      initial_delay = 15;
+      period = 30;
+    };
+    restart = {
+      on = "always";
+      max = null;
+    };
+  };
+
+  processes.wasm_release = {
+    exec = "trunk serve --release --port 8081";
+    ready = {
+      http.get = {
+        port = 8081;
+        path = "/";
+      };
+      initial_delay = 15;
+      period = 30;
+    };
+    restart = {
+      on = "always";
+      max = null;
+    };
+  };
 }
